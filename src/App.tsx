6560a4fe8624a85e1eda76510f927452373dd269
pancakeslmao <phucthin29@gmail.com>
2025-03-16 12:34:16 +0700

    Update: folder structure, 7.3 info

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import elysiaLogo from './assets/elysia.png'
-import elysiaAvatar from './assets/ely.png'
-import './App.css'
-import Footer from './components/Footer'
+import { useState } from "react";
+
+import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="inline-flex">
-        <a className="stack_icon" href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a className="stack_icon" href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a className="stack_icon" href="https://elysiajs.com/" target="_blank">
-          <img src={elysiaLogo} className="logo" alt="Elysia logo" />
-        </a>
-      </div>
-      <h1>Vite + React + Elysia</h1>
-      <img src={elysiaAvatar} className="ely_ava" alt="React logo" />
-      <div className="card">
-        <button className='sub_btn' onClick={() => setCount((count) => count + 1)}>
-          Elycon: {count}
-        </button>
-      </div>
-      <p className="read-the-docs">Click on each logo to learn more</p>
       <Footer />
     </>
   );
 }
 
-export default App
+export default App;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,6 @@
-import elysiaJS from '../assets/elysia_v.webp';
+import reactLogo from "../assets/react.svg";
+import viteLogo from "/vite.svg";
+import elysiaLogo from "../assets/elysia.png";
 export default function Footer() {
   return (
     <footer>
@@ -6,12 +8,39 @@ export default function Footer() {
         <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
-              <a href="https://elysiajs.com/" className="flex items-center">
-                {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                  
-                </span> */}
-                <img src={elysiaJS} className="h-24 me-3" alt="FlowBite Logo" />
-              </a>
+              <div className="flex flex-col">
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                  Built with
+                </span>
+
+                <div className="inline-flex">
+                  <a
+                    className="stack_icon"
+                    href="https://vite.dev"
+                    target="_blank"
+                  >
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                  </a>
+                  <a
+                    className="stack_icon"
+                    href="https://react.dev"
+                    target="_blank"
+                  >
+                    <img
+                      src={reactLogo}
+                      className="logo react"
+                      alt="React logo"
+                    />
+                  </a>
+                  <a
+                    className="stack_icon"
+                    href="https://elysiajs.com/"
+                    target="_blank"
+                  >
+                    <img src={elysiaLogo} className="logo" alt="Elysia logo" />
+                  </a>
+                </div>
+              </div>
             </div>
             <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
               <div>
@@ -20,8 +49,8 @@ export default function Footer() {
                 </h2>
                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                   <li className="mb-4">
-                    <a href="https://flowbite.com/" className="hover:underline">
-                      Flowbite
+                    <a href="#" className="hover:underline">
+                      React
                     </a>
                   </li>
                   <li>
