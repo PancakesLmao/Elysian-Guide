fab68364050f8286d126f3b80a76d9f4bd37a7f1
pancakeslmao <phucthin29@gmail.com>
2025-03-17 23:03:51 +0700

    Updated: Finished character gallery section

--- src/components/CardsDisplay.tsx
+++ src/components/CardsDisplay.tsx
@@ -1,9 +1,11 @@
-import flameChasers from '../assets/flame_chasers/cards.json';
-import React, { useState } from 'react';
-import './cardsDisplay.css'
+import flameChasers from "../assets/flame_chasers/cards.json";
+import React, { useState } from "react";
+import "./cardsDisplay.css";
+
 interface FlameChaserData {
   name: string;
   ranking: number;
+  numeral: string;
   signetName: string;
   imageSource: string;
 }
@@ -11,36 +13,43 @@ interface FlameChaserData {
 const FlameChaserData: FlameChaserData[] = flameChasers;
 
 export default function CardList() {
-    const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
-    return (
-      <>
-        <div className="gallery-container w-full overflow-x-hidden">
-          <div className="character-gallery flex overflow-x-auto">
-            {FlameChaserData.map((character, index) => (
+  return (
+    <>
+      <div className="gallery-container w-full overflow-x-hidden">
+        <div className="character-gallery flex overflow-x-auto">
+          {FlameChaserData.map((character, index) => (
+            <div
+              key={character.name}
+              className={`character-card relative overflow-hidden ${
+                activeIndex === index ? "active" : ""
+              }`}
+              onMouseEnter={() => setActiveIndex(index)}
+              onMouseLeave={() => setActiveIndex(null)}
+            >
               <div
-                key={character.name}
-                className={`character-card relative ${
+                className={`card-upper-overlay absolute ${
                   activeIndex === index ? "active" : ""
                 }`}
-                onMouseEnter={() => setActiveIndex(index)}
-                onMouseLeave={() => setActiveIndex(null)}
-              >
-                <div className="card-overlay absolute text-white">
-                  <div className="character-info">
-                    <span className="character-rank">#{character.ranking}</span>
-                    <p className="signet-name">{character.signetName}</p>
-                  </div>
+              ></div>
+              <div className="card-overlay absolute text-white">
+                <div className="character-info text-center">
+                  <span className="character-rank text-[3rem]">
+                    {character.numeral}
+                  </span>
+                  <p className="signet-name text-[27px] w-[60%] h-full">{character.signetName}</p>
                 </div>
-                <img
-                  src={character.imageSource}
-                  alt={`${character.name} - Flame Chaser #${character.ranking}`}
-                  className="character-image object-cover"
-                />
               </div>
-            ))}
-          </div>
+              <img
+                src={character.imageSource}
+                alt={`${character.name}`}
+                className="character-image object-cover"
+              />
+            </div>
+          ))}
         </div>
-      </>
-    );
+      </div>
+    </>
+  );
 }
